6b7d7434fc0af5a1c536df40d7689d90a0f5d41c
Lucas Ferreira <lucasfalvesferreira@gmail.com>
2024-12-03 10:03:58 -0300

    Board and Piece git status

---
+++ src/boardgame/Board.java
@@ -0,0 +1,20 @@
+package boardgame;
+
+public class Board {
+
+	private int rows;
+	private int columns;
+	private Piece[][] pieces;
+	
+	public Board() {
+	}
+
+	public Board(int rows, int columns) {
+		super();
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Piece[rows][columns];
+	}
+	
+	
+}
