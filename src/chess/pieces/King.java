69a3da90cea04844b1709ebf8777a8832dcee51f
Lucas Ferreira <lucasfalvesferreira@gmail.com>
2024-12-03 13:45:10 -0300

    BoardException and defensive programming

---
+++ src/chess/pieces/King.java
@@ -0,0 +1,25 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class King extends ChessPiece{
+
+	private Color color;
+	public King(Board board, Color color) {
+		super(board, color);
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		if(color == Color.WHITE) {
+			return "♔";
+		}
+		else {
+			return "♚";
+		}
+	}
+	
+}
