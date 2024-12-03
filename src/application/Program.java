ffdfd8b24ee14a486b6a525755851ece3f10211a
Lucas Ferreira <lucasfalvesferreira@gmail.com>
2024-12-03 09:55:39 -0300

    Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");	
 	
 	
+		Position ps = new Position(3,2);
+		System.out.println(ps);
 	}
 }
